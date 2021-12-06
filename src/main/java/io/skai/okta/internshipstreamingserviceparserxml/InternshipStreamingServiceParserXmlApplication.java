8d592226fb627e3bb87d710549773851f3f6ec7e
ivan <ivan.linnik.1990@gmail.com>
2021-12-06 12:32:04 +0200

    init commit

---
+++ src/main/java/io/skai/okta/internshipstreamingserviceparserxml/InternshipStreamingServiceParserXmlApplication.java
@@ -0,0 +1,13 @@
+package io.skai.okta.internshipstreamingserviceparserxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipStreamingServiceParserXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipStreamingServiceParserXmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/skai/okta/internshipstreamingserviceparserxml/InternshipStreamingServiceParserXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.skai.okta.internshipstreamingserviceparserxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipStreamingServiceParserXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
