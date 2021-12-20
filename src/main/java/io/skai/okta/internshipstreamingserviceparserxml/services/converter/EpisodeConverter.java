50683a04061bae70c0f1f14344aaa1058004a210
Ivan Linnik <ivan.linnik@kenshoo.com>
2021-12-20 12:50:01 +0200

    add regex in EpisodeConverter.java

--- src/main/java/io/skai/okta/internshipstreamingserviceparserxml/services/converter/EpisodeConverter.java
+++ src/main/java/io/skai/okta/internshipstreamingserviceparserxml/services/converter/EpisodeConverter.java
@@ -4,13 +4,22 @@ import io.skai.okta.internshipstreamingserviceparserxml.dto.Episode;
 import io.skai.okta.internshipstreamingserviceparserxml.dto.LostFilmRssItem;
 import org.springframework.stereotype.Component;
 
+import java.security.InvalidParameterException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public class EpisodeConverter {
+    private final static Pattern TVS_TITLE_PATTERN = Pattern.compile("(?<=\\().+?(?=\\))");
+    private final static Pattern SEASON_PATTERN = Pattern.compile("(?<=\\(S)\\d{2}(?=E\\d{2})");
+    private final static Pattern EPISODE_PATTERN = Pattern.compile("(?<=\\(S\\d{2}E)\\d{2}(?=\\))");
+
     public Episode map(LostFilmRssItem lostFilmRssItem) {
         String title = lostFilmRssItem.getTitle();
+
+        String tvSeriesTitle = extract(title, "tvsTitle");
         int season = Integer.parseInt(extract(title, "season"));
         int episodeNumber = Integer.parseInt(extract(title, "episode"));
-        String tvSeriesTitle = extract(title, "tvsTitle");
 
         return Episode.builder()
                       .season(season)
@@ -24,17 +33,40 @@ public class EpisodeConverter {
     }
 
     private String extract(String from, String value) {
-        final String[] result = from.replaceAll("\\(", ")").split("\\)");
         switch (value) {
             case "tvsTitle":
-                return result[1];
+                return extractTvsTitle(from);
             case "season":
-                return result[3].substring(1, 3);
+                return extractSeason(from);
             case "episode":
-                return result[3].substring(4, 6);
+                return extractEpisode(from);
             default:
                 throw new IllegalStateException("Unexpected value: " + value);
         }
+    }
+
+    private String extractTvsTitle(String from) {
+        Matcher matcher = TVS_TITLE_PATTERN.matcher(from);
+        if (matcher.find()) {
+            return matcher.group(0);
+        }
+        throw new InvalidParameterException("Format of \"title\" string is illegal");
+    }
+
+    private String extractSeason(String from) {
+        Matcher matcher = SEASON_PATTERN.matcher(from);
+        if (matcher.find()) {
+            return matcher.group(0);
+        }
+        throw new InvalidParameterException("Format of \"title\" string is illegal");
+    }
+
+    private String extractEpisode(String from) {
+        Matcher matcher = EPISODE_PATTERN.matcher(from);
+        if (matcher.find()) {
+            return matcher.group(0);
+        }
+        throw new InvalidParameterException("Format of \"title\" string is illegal");
     }
 
 }
